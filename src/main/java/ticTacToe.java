27ada0ab3a15a612be8ec95766147b3db486d115
Zahra <zahram1087@gmail.com>
2018-11-20 23:33:22 -0800

    tic-tac-toe

---
+++ src/main/java/ticTacToe.java
@@ -0,0 +1,79 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class ticTacToe {
+
+    public static void main (String[] args){
+        pushMethod ();
+//        char [] pos = {' ',' ',' ',' ',' ',' ',' ',' ',' '};
+//
+//        Scanner sc = new Scanner(System.in);
+//        int input;
+//        char turn = 'X';
+//        int currentTurn = 1;
+//           while(true) {
+//
+//               do {
+//
+//                   System.out.println("Welcome let's play, the rules are: ");
+//
+//                   System.out.println(" " + pos[6] + "| " + pos[7] + "  | " + pos[8]);
+//                   System.out.println("--+----+--");
+//                   System.out.println(" " + pos[3] + "| " + pos[4] + "  | " + pos[5]);
+//                   System.out.println("--+----+--");
+//                   System.out.println(" " + pos[0] + "| " + pos[1] + "  | " + pos[2]);
+//
+//                   System.out.print("player one choose a position between 1-8: ");
+//                   input = sc.nextInt();
+//               } while (pos[input - 1] == 'X' || pos[input - 1] == 'O');
+//
+//               pos[input - 1] = turn;
+//
+//               System.out.println(" " + pos[6] + "| " + pos[7] + "  | " + pos[8]);
+//               System.out.println("--+----+--");
+//               System.out.println(" " + pos[3] + "| " + pos[4] + "  | " + pos[5]);
+//               System.out.println("--+----+--");
+//               System.out.println(" " + pos[0] + "| " + pos[1] + "  | " + pos[2]);
+//
+//               if(pos[0]==turn && pos[1] == turn && pos[2]==turn
+//                       ||pos[3]==turn && pos[4] == turn && pos[5]==turn
+//                       || pos[6]==turn && pos[7] == turn && pos[8]==turn
+//                       || pos[0]==turn && pos[3] == turn && pos[6]==turn
+//                       || pos[0]==turn && pos[4] == turn && pos[7]==turn
+//                       ||pos[2]==turn && pos[5] == turn && pos[8]==turn
+//                       || pos[2]==turn && pos[4] == turn && pos[7]==turn) {
+//                   System.out.println(turn + " YOU WON!");
+//                   break;
+//               }
+////
+//
+//               if (turn == 'X') {
+//                   turn = 'O';
+//               } else if (turn == 'O') {
+//                   turn = 'X';
+//
+//                }
+//               currentTurn++;
+//               if(currentTurn>9){
+//                   System.out.println("No Winners Today!");
+//                   break;
+//               }
+
+//           }
+
+        }
+        
+    }
+
+
+
+
+
+
+
+//  |    |
+//---+---+---
+//  |    |
+//---+---+---
+//  |    |
